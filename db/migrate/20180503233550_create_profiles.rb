352a5b7dc1854f167d3b9235afe325f1044e7481
Tim Moon <tmoon@Tims-MBP.lan>
2018-05-04 09:54:23 +1000

    create pages controller with home view

--- db/migrate/20180503233550_create_profiles.rb
+++ db/migrate/20180503233550_create_profiles.rb
@@ -3,7 +3,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
     create_table :profiles do |t|
       t.references :user, foreign_key: true
       t.string :first_name
-      t.string :last_name,
+      t.string :last_name
       t.text :image_data
       t.string :phone
       t.string :street_address
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503060323) do
+ActiveRecord::Schema.define(version: 20180503233550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 20180503060323) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.text "image_data"
+    t.string "phone"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "postcode"
+    t.string "country_code"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "sub_categories", force: :cascade do |t|
@@ -46,5 +64,6 @@ ActiveRecord::Schema.define(version: 20180503060323) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
   add_foreign_key "sub_categories", "main_categories"
 end
