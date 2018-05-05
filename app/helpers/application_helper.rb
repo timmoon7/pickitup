6657dbfb6d06f18821facf5538d5f4a5237325aa
Tim Moon <tim.moon7@gmail.com>
2018-05-05 21:05:25 +1000

    create geocoder in profile helper and show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def google_map_image_tag(profile)
+        image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{@profile.latitude},#{@profile.longitude}&markers=#{@profile.latitude},#{@profile.longitude}&zoom=10&size=400x320&key=AIzaSyA4BHW3txEdqfxzdTlPwaHsYRSZbfeIcd8"    
+    end 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,24 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates(
+    :first_name, 
+    :last_name, 
+    :street_address, 
+    :city, 
+    :postcode, 
+    :state, 
+    :country_code, 
+    presence: true)
+
+  geocoded_by :full_address
+  after_validation :geocode
+    
+  def full_address
+    "#{street_address}, #{city}, #{state}, #{postcode}, #{country_code}"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
