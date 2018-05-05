d771c9a417986f81cb7a3c6427e2bd7765851a69
Tim Moon <tim.moon7@gmail.com>
2018-05-06 00:57:01 +1000

    create shirne for profile image upload

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -71,6 +71,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :image_data, :phone, :street_address, :city, :state, :postcode, :country_code)
+      params.require(:profile).permit(:first_name, :last_name, :image, :phone, :street_address, :city, :state, :postcode, :country_code)
     end
 end
