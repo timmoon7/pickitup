0a501c20be0a83575e119724314087147aef85b6
Tim Moon <tim.moon7@gmail.com>
2018-05-06 14:54:22 +1000

    delete images in cash and store folders

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -66,9 +66,7 @@ class ProfilesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      id = current_user.profile.id 
-      @profile = Profile.find(id)
-      # @profile = Profile.find(params[:id])
+      @profile = Profile.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
