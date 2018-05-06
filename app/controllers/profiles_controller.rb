2351aaf2501b72eae8050558c9daba9542bc4abd
Tim Moon <tim.moon7@gmail.com>
2018-05-06 14:49:42 +1000

    create link to edit profile from home page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -66,7 +66,9 @@ class ProfilesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      @profile = Profile.find(params[:id])
+      id = current_user.profile.id 
+      @profile = Profile.find(id)
+      # @profile = Profile.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
