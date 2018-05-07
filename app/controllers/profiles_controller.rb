eb11fcc8b0cd219814af89899dee851dbdd5f7c5
Tim Moon <tim.moon7@gmail.com>
2018-05-07 22:05:59 +1000

    Change Shrine to AWS S3

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,7 +19,7 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/1/edit
   def edit
-    @profile = Profile.find_or_initialize_by(user: current_user) 
+    # @profile = Profile.find_or_initialize_by(user: current_user) 
   end
 
   # POST /profiles
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,10 +3,10 @@ require "shrine"
 require "shrine/storage/s3"
 
 s3_options = {
-  access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
-  secret_access_key: ENV.fetch('S3_SECRET_ACCESS_KEY'),
-  region:            ENV.fetch('S3_REGION'),
-  bucket:            ENV.fetch('S3_BUCKET'),
+  access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+  secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  region:            ENV.fetch('AWS_REGION'),
+  bucket:            ENV.fetch('AWS_BUCKET'),
 }
 
 Shrine.storages = {
@@ -14,13 +14,5 @@ Shrine.storages = {
   store: Shrine::Storage::S3.new(**s3_options),
 }
 
-
-# require "shrine/storage/file_system"
-
-# Shrine.storages = {
-#   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-#   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
-# }
-
-# Shrine.plugin :activerecord
-# Shrine.plugin :cached_attachment_data # for forms
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data # for forms
