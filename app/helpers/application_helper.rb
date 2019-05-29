42fc1cddb9541be1a4b18f7debdff5ad26f67e98
Tim Moon <tim.moon7@gmail.com>
2019-05-30 02:27:56 +1000

    Change setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
     def google_map_image_tag(profile)  
         api_key = ENV.fetch('GOOGLE_MAP_KEY')
-        map_uri = "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=#{api_key}" 
-        image_tag map_uri.force_encoding("UTF-8")
+        image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=#{api_key}"    
     end 
 end
