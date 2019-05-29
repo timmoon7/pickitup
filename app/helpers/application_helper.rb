fb12e0e0061a75c98320e7b365667ba4acd565e7
Tim Moon <tim.moon7@gmail.com>
2019-05-30 01:54:30 +1000

    Hide Google Map API Key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-    def google_map_image_tag(profile)
-        # image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=AIzaSyA4BHW3txEdqfxzdTlPwaHsYRSZbfeIcd8"    
-        image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=AIzaSyDolmJy0YpUryP2_jpsw1KI7i8_cV7hNuU"    
+    def google_map_image_tag(profile)  
+        image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=#{google_map_key}"    
     end 
 end
+str = str.force_encoding("UTF-8")
