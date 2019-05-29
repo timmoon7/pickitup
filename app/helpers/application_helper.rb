de1ebc1f6e59bc0cf12966cdd862edf6900257b5
Tim Moon <tim.moon7@gmail.com>
2019-05-30 01:28:36 +1000

    Change geocoder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
     def google_map_image_tag(profile)
-        image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=AIzaSyA4BHW3txEdqfxzdTlPwaHsYRSZbfeIcd8"    
+        # image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=AIzaSyA4BHW3txEdqfxzdTlPwaHsYRSZbfeIcd8"    
+        image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=AIzaSyDolmJy0YpUryP2_jpsw1KI7i8_cV7hNuU"    
     end 
 end
