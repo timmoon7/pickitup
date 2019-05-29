6774ffad38df63a5101a6d63037abe8b7bddd716
Tim Moon <tim.moon7@gmail.com>
2019-05-30 02:05:27 +1000

    Change GeoCoding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
     def google_map_image_tag(profile)  
-        image_tag "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=#{google_map_key}"    
+        api_key = ENV.fetch('GOOGLE_MAP_KEY')
+        map_uri = "https://maps.googleapis.com/maps/api/staticmap?center=#{profile.latitude},#{profile.longitude}&markers=#{profile.latitude},#{profile.longitude}&zoom=13&size=400x300&key=#{api_key}" 
+        image_tag map_uri.force_encoding("UTF-8")
     end 
 end
-str = str.force_encoding("UTF-8")
